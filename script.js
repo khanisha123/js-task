d21469063b1a723cdb68a6c0326555cb7d9eba78
khanisha123 <khanisha@code.edu.az>
2021-11-26 03:11:37 +0400

    js task

---
+++ script.js
@@ -0,0 +1,23 @@
+// let arr=[1,2,3,4,5,6,7,8,9,17]
+
+// arr.forEach(element => {
+    
+//     if (element%2==0) {
+        
+//         console.log(element)
+
+//     }
+    
+// });
+
+// let num=12;
+// let array=[1,2,3,4,12,13,14,15,20,25]
+// array.forEach(number => {
+    
+//     if (number==num) {
+//         console.log("true");
+//     }
+    
+    
+    
+// });
