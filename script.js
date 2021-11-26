bca7a92e40809c9cfe2d1a99bf3a442ecb79d95a
khanisha123 <khanisha@code.edu.az>
2021-11-26 13:46:28 +0400

    Fix colculater

--- script.js
+++ script.js
@@ -10,21 +10,21 @@
     
 // });
 
-// let num=12;
-// let array=[1,2,3,4,12,13,14,15,20,25]
-// array.forEach(number => {
+let num=15;
+let array=[1,2,3,4,12,13,14,15,20,25]
+array.forEach(number => {
     
-//     if (number==num) {
-//         console.log("true");
-//     }
-    
-    
-    
-// });
+    if (number==num) {
+        console.log("true");
+        break;
+    }
+   
+});
 
-// let number1;
-// let number2;
-// let x=0;
+// let number1=4;
+// let number2=92;
+// let sum=0;
+// let x="+";
 // switch (x) {
 //     case '+': 
 //     sum=number1+number2   
